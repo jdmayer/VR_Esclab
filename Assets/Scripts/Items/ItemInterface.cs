2e408e83ee754d7035aef2c4b1505b161dac8891
Sadeiko <xovval@gmail.com>
2021-10-13 22:47:03 +0200

    Added skybox and initial scripts

---
+++ Assets/Scripts/Items/ItemInterface.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInterface : MonoBehaviour
+{
+    int value;
+    int weight; //for sound amplitude when dropping
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
