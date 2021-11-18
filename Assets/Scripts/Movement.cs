a450a928510d072bc63a4749dbbe20ee834b5d88
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-11-18 22:56:45 +0100

    Add movement script to move object

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Author: Janine Mayer
+/// Script to move object forward, backward and enable rotation around y-axis (yaw)
+/// Used to test functionality of character and items without hmd hardware
+/// </summary>
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(transform.position, new Vector3(0, 1, 0), moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(transform.position, new Vector3(0, 1, 0), -moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+        }
+    }
+}
