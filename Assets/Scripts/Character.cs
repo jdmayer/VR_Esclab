58cc8be956c8f84cb3f34fb2b20d34b92fac8b2b
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-11-18 22:19:16 +0100

    Add basic character script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Janine Mayer
+/// </summary>
+public class Character 
+{
+
+    private int currHealth;
+    private int maxHealth;
+
+    private bool isHoldingItem;
+
+    public void SetCurrHealth(int newCurrHealth)
+    {
+        this.currHealth = newCurrHealth;
+    }
+
+    public void ChangeCurrHealth(int healthDifference)
+    {
+        this.currHealth += healthDifference;
+    }
+
+    public int GetCurrHealth()
+    {
+        return this.currHealth;
+    }
+
+    public void SetMaxHealth(int newMaxHealth)
+    {
+        this.maxHealth = newMaxHealth;
+    }
+
+    public int GetMaxHealth()
+    {
+        return this.maxHealth;
+    }
+
+    public Character(int maxHealth)
+    {
+        this.SetMaxHealth(maxHealth);
+        this.SetCurrHealth(maxHealth);
+    }
+
+    public Character() : this(100)
+    {
+    }
+}
