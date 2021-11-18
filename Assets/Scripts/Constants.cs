f8e911fc24f747b359389d9c7b35fb2a99b7747c
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-11-19 00:20:06 +0100

    Add constant class to save strings

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Author: Janine Mayer
+/// Class containing all needed constants to void string cluttering within project
+/// </summary>
+namespace Assets.Scripts
+{
+    public static class Constants
+    {
+        public static string player = "Player";
+        public static string manualPlayer = "ManualPlayer";
+
+        public static string[] labyrinthScenes = { 
+            "Labyrinth_A", 
+            "Labyrinth_B" 
+        };
+
+    }
+}
