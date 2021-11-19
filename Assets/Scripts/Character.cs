ccd31fa09e5a0dddc06e56bdeb104407db9bc583
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-11-19 01:03:11 +0100

    Add painlevel and healthcheck

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,17 +10,19 @@ public class Character
 
     private int currHealth;
     private int maxHealth;
+    private int painLevel;
 
     private bool isHoldingItem;
 
     public void SetCurrHealth(int newCurrHealth)
     {
+        CheckHealthCondition(currHealth, newCurrHealth);
         this.currHealth = newCurrHealth;
     }
 
     public void ChangeCurrHealth(int healthDifference)
     {
-        this.currHealth += healthDifference;
+        SetCurrHealth(this.currHealth + healthDifference);
     }
 
     public int GetCurrHealth()
@@ -38,6 +40,18 @@ public class Character
         return this.maxHealth;
     }
 
+    public void SetPainLevel(int painLevel)
+    {
+        this.painLevel = painLevel < maxHealth && painLevel > 0 
+            ? painLevel
+            : maxHealth / 2;
+    }
+
+    public int GetPainLevel()
+    {
+        return this.painLevel;
+    }
+
     public Character(int maxHealth)
     {
         this.SetMaxHealth(maxHealth);
@@ -46,5 +60,20 @@ public class Character
 
     public Character() : this(100)
     {
+    }
+
+    private void CheckHealthCondition(int oldHealth, int newHealth)
+    {
+        if (oldHealth > painLevel && newHealth < painLevel)
+        {
+            // blurr vision
+            // adjust audio
+            Debug.Log("Character is in critical condition! " + currHealth + " / " + maxHealth);
+        }
+        else if (oldHealth < painLevel && newHealth > painLevel)
+        {
+            // stop all distraction
+            Debug.Log("Character is healed! " + currHealth + " / " + maxHealth);
+        }
     }
 }
