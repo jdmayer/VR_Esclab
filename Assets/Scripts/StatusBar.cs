fb0588551d546aafaab215a24796108b0b8bb0e6
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-11-28 16:41:11 +0100

    Add script to position statusbar close to leftController
    Removed code from statusbar script as it would execute the code as often as there are statusbars

--- Assets/Scripts/StatusBar.cs
+++ Assets/Scripts/StatusBar.cs
@@ -1,7 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Valve.VR;
 
 /// <summary>
 /// Author: Janine Mayer
@@ -10,38 +9,16 @@ using Valve.VR;
 public class StatusBar : MonoBehaviour
 {
     public Image Bar;
-    public Camera Camera;
 
     public TextMeshProUGUI CurrText;
     public TextMeshProUGUI MaxText;
 
-    public SteamVR_Action_Boolean Display;
-    public SteamVR_Input_Sources HandType;
-
-    private Canvas parentCanvas;
-
     void Start()
     {
-        Display.AddOnStateDownListener(DisplayStatsBar, HandType);
-        Display.AddOnStateUpListener(HideStatsBar, HandType);
-
-        parentCanvas = gameObject.GetComponentInParent<Canvas>();
-        parentCanvas.enabled = false;
     }
 
     private void Update()
-    {
-        parentCanvas.transform.LookAt(Camera.transform);
-    }
-
-    private void DisplayStatsBar(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        parentCanvas.enabled = true;
-    }
-
-    private void HideStatsBar(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        parentCanvas.enabled = false;
+    {        
     }
 
     public void UpdateStatBar(float fraction, int currValue, int maxValue)
---
+++ Assets/StatusbarPositioning.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using Valve.VR;
+
+/// <summary>
+/// Author: Janine Mayer
+/// Adjust visibility and positionof canvas with statusbars depending on controller input and position
+/// </summary>
+public class StatusbarPositioning : MonoBehaviour
+{
+    public Camera Camera;
+
+    public SteamVR_Action_Boolean Display;
+    public SteamVR_Input_Sources HandType;
+
+    public GameObject leftController;
+
+    private Canvas canvasObject = null;
+
+    void Start()
+    {
+        Display.AddOnStateDownListener(DisplayStatsBar, HandType);
+        Display.AddOnStateUpListener(HideStatsBar, HandType);
+
+        canvasObject = gameObject.GetComponentInParent<Canvas>();
+        canvasObject.enabled = false;
+    }
+
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(
+            leftController.transform.position.x + 0.5f, 
+            leftController.transform.position.y + 0.5f, 
+            leftController.transform.position.z);
+
+        gameObject.transform.LookAt(Camera.transform);  
+    }
+
+
+    private void DisplayStatsBar(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        canvasObject.enabled = true;
+    }
+
+    private void HideStatsBar(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        canvasObject.enabled = false;
+    }
+}
